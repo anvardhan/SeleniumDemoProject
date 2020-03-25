9beec689d7e8546d167ef705fa4cedc24bc2a37f
Anand Vardhan <anand.vn100@gmail.com>
2020-03-24 20:52:38 -0500

    Added RegisterPageTest java file and added a Test by PersonA

---
+++ src/test/java/com/freecrm/testcases/RegisterPageTest.java
@@ -0,0 +1,16 @@
+package com.freecrm.testcases;
+
+import org.testng.annotations.Test;
+
+public class RegisterPageTest {
+	
+	// RegisterPageTest.java file is added by Person A and also registerTest is added by Person A
+	
+	@Test
+	public void resisterTest() {
+		
+		System.out.println("This is register test and this test is added by Person A");
+		
+	}
+
+}
