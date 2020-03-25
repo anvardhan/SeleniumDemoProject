6027d6583e9f1137303632144f46f0546d31b3a1
Anand Vardhan <anand.vn100@gmail.com>
2020-03-24 23:23:34 -0500

    Commited by Person B in develop branch

---
+++ src/test/java/com/freecrm/testcases/NewClassTest.java
@@ -0,0 +1,14 @@
+package com.freecrm.testcases;
+
+import org.testng.annotations.Test;
+
+public class NewClassTest {
+	
+	@Test
+	public void newTest() {
+		
+		//This class and code is Added by Person B on develop branch
+		System.out.println("This test is added through develop branch and yet to be tested");
+	}
+
+}
